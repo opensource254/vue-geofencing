0c93497fb388c8239c152c20ead511e74f0a30c2
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-11 15:31:42 +0300

    initial app config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+import VSocket from "vue-socket.io";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyDBS6ORL_N5Q-EHwmS5c4kEeiQPTCm_xsE",
+    libraries: "geometry" // This is required if you use the Autocomplete
+  }
+});
+
+Vue.use(
+  new VSocket({
+    debug: true,
+    connection: "http://192.168.254.72:4001"
+  })
+);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
